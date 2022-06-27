fbc4b59800e5fa0dfe22fafe9303cbe1066b6761
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-06-27 14:10:58 +0300

    Update users_bp.py

--- auth_api/users_bp.py
+++ auth_api/users_bp.py
@@ -1,4 +1,14 @@
 from flask import Blueprint, render_template, request
 from flask.json import jsonify
-
+from db_models import  User, Group
 users_bp = Blueprint("users_bp", __name__)
+
+@users_bp.route("/", methods=["GET"])
+def list_groups():
+    """
+    Список всех пользователей
+    """
+    users = []
+    for user in User.query.all():
+        users.append(user.to_json())
+    return jsonify(users)
