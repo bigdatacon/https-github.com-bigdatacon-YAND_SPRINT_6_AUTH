72e7384fc8ef64f22fb0e6c0bf7846195bb8f9bf
root <root@ovz4.j54523970.pv29m.vps.myjino.ru>
2022-06-29 16:42:50 +0300

    Uncomment JWT settings

--- auth_api/auth_config.py
+++ auth_api/auth_config.py
@@ -12,16 +12,17 @@ import os
 import shutil
 import sys
 import time
+from datetime import timedelta
 from flask import Flask
 
 class Config:
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://postgres:123@127.0.0.1:5432/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    # JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
-    # JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
-    # ACCESS_EXPIRES = timedelta(hours=1)
+    JWT_SECRET_KEY = "jwt"
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+    ACCESS_EXPIRES = timedelta(hours=1)
     SWAGGER_TEMPLATE = {
         "securityDefinitions": {
             "APIKeyHeader": {
