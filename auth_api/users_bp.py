8182c0dbd643db430416ba72ec477ef5da457ec9
root <root@ovz4.j54523970.pv29m.vps.myjino.ru>
2022-07-02 20:48:25 +0300

    Logout works

--- auth_api/users_bp.py
+++ auth_api/users_bp.py
@@ -194,11 +194,11 @@ def get_user_history(**kwargs):
     return jsonify([h.to_json() for h in history])
 
 
-# @jwt.token_in_blocklist_loader
-# def check_if_token_is_revoked(jwt_header, jwt_payload):
-#     jti = jwt_payload["jti"]
-#     token_in_redis = jwt_redis_blocklist.get(jti)
-#     return token_in_redis is not None
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload):
+    jti = jwt_payload["jti"]
+    token_in_redis = jwt_redis_blocklist.get(jti)
+    return token_in_redis is not None
 
 
 """тестовые функции"""
